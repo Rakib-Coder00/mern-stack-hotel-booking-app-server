10cc56b60e3792ffe177300d5c38b10f02e47d81
unknown <rhbappy456@gmail.com>
2022-07-01 22:02:39 +0600

    get all rooms from server

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const dbConfig = require('./db');
+const roomsRoute = require('./routes/roomsRoute');
+
+app.use('/api/rooms', roomsRoute);
 const port = process.env.PORT || 5000;
 
 //Middleware ==>
-app.use(cors());
+// app.use(cors());
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Express Server is running...🔥');
-})
+// app.get('/', (req, res) => {
+//     res.send('Express Server is running...🔥');
+// })
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
